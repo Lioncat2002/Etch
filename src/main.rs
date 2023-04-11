73d6fdef1ce533c0f948ab1643a6ac19286a62e0
Lioncat2002 <sankhayan2002@gmail.com>
2023-04-12 02:37:01 +0530

    feat: init hello world api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::Rocket;
+use rocket::Build;
+use rocket::get;
+use rocket::launch;
+use rocket::routes;
+#[get("/bruh")]
+fn index() -> &'static str {
+    println!("YOoo");
+    "Hello World!"
+}
+
+#[launch]
+fn rocket() -> Rocket<Build> {
+    rocket::build().mount("/", routes![index])
+    //3rocket::build().mount("/", routes![index])
 }
